f81e40ef7516c79e64a1801f9baf0d3f83f10de7
Antares <sorryiwin.llc@gmail.com>
2025-10-04 14:42:00 +0300

    0.4.0 Beta Files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.edu.axemoobile',
-  appName: 'AxeMobile',
+  appId: 'app.lovable.911e8d2b9f86490dabb8122918eb7a16',
+  appName: 'axe-miner-hub',
   webDir: 'dist',
   server: {
-    url: 'https://localhost:8080',
+    url: 'https://911e8d2b-9f86-490d-abb8-122918eb7a16.lovableproject.com?forceHideBadge=true',
     cleartext: true
   },
   plugins: {
