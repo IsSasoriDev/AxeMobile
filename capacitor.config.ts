11b5e7981545e81a9bfc78bbafb5204290bd8f67
edu <sorryiwin.llc@gmail.com>
2025-12-05 22:35:59 +0200

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.911e8d2b9f86490dabb8122918eb7a16',
-  appName: 'axe-miner-hub',
+  appId: 'com.eduionita.axemobile',
+  appName: 'AxeMobile',
   webDir: 'dist',
   server: {
-    url: 'https://911e8d2b-9f86-490d-abb8-122918eb7a16.lovableproject.com?forceHideBadge=true',
+    url: 'http://localhost:3000/',
     cleartext: true
   },
   plugins: {
